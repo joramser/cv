98cd9b9dba1503629692fda565a0ff06ad80cf6a
Jorge Ramírez Serrato <jramirezserrato@gmail.com>
2025-12-28 16:55:04 +0200

    Disable flickering cursor for now

--- apps/tui/src/routes/intro.tsx
+++ apps/tui/src/routes/intro.tsx
@@ -4,19 +4,19 @@ import { isSSH } from "@tui/utils";
 import { Box, Newline, Text, useInput } from "ink";
 import Link from "ink-link";
 import open from "open";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 const ABOUT_URL = `${data.contact.website}/about`;
 
 export const Intro = () => {
-  const [visible, setVisible] = useState(true);
+  // const [visible, setVisible] = useState(true);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setVisible((visible) => !visible);
-    }, 500);
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setVisible((visible) => !visible);
+  //   }, 500);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   useInput((input) => {
     if (input === "o") {
@@ -25,14 +25,19 @@ export const Intro = () => {
   });
 
   return (
-    <Box flexDirection="column" padding={1} justifyContent="space-between" height="100%">
+    <Box
+      flexDirection="column"
+      padding={1}
+      justifyContent="space-between"
+      height="100%"
+    >
       <Box flexDirection="column">
         <Box>
           <Text bold color="magenta">
             {data.name}
           </Text>
           <Text color="blue"> ({data.handle}) </Text>
-          {visible && <Text backgroundColor="red"> </Text>}
+          {/*{visible && <Text backgroundColor="red"> </Text>}*/}
         </Box>
         <Text color="green">{data.title}</Text>
         <Text color="cyan">📍{data.contact.location}</Text>
